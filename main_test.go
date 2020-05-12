76cddb3b586b076170d7b297d8b09dafc21ce63b
Charles Ashby <charlesa.l@hotmail.com>
2020-05-11 20:40:43 -0700

    feat(parser): end to end flow + test (parse file, write to buffer, send to api)
    * feat(parser): push data to api
    
    * feat(parser): push data to api
    
    * chore(test): added end to end test (parse file, write to buffer, send to api)

--- main_test.go
+++ main_test.go
@@ -1,46 +1,152 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/hpcloud/tail"
+	"github.com/trivago/grok"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 )
 
-func GenerateJWT() (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
+func isAuthorized(r *http.Request, secret string) bool {
+	if r.Header["Token"] != nil {
 
-	claims := token.Claims.(jwt.MapClaims)
+		token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("bad token")
+			}
+			return secret, nil
+		})
 
-	claims["authorized"] = true
+		if err != nil {
+			return false
+		}
 
-	tokenString, err := token.SignedString([]byte(os.Getenv("MONITORO_SECRET_KEY")))
-
-	if err != nil {
-		fmt.Errorf("Something Went Wrong: %s", err.Error())
-		return "", err
+		if token.Valid {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
 	}
-
-	return tokenString, nil
 }
 
-func TestFormatDiscovery(t *testing.T) {
+func MockHandler(w http.ResponseWriter, r *http.Request) {
+	if !isAuthorized(r, os.Getenv("MONITORO_SECRET_KEY")) {
+		fmt.Errorf("Bad key")
+		return
+	}
+
+}
+
+func Map(vs []map[string]string, f func(map[string]string) bool) []bool {
+	vsm := make([]bool, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+func contains(arr [3]string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+func assertReqFields(fields map[string]string) bool {
+	reqFields := []string{"verb", "IP", "response", "request", "timestamp"}
+	for _, f := range reqFields {
+		if _, ok := fields[f]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func TestEndToEnd(t *testing.T) {
 	files := map[string]string{
 		"tests/apache.log":          "%{COMMONAPACHELOG}",
 		"tests/apache_combined.log": "%{COMBINEDAPACHELOG}",
 		"tests/common_log.log":      "%{COMMONAPACHELOG}",
-		"tests/file.log":            "%{UNKNOWNFORMAT}",
 	}
-	for logfile, expFmt := range files {
-		recFmt, err := FormatDiscovery(logfile)
+	for file, pattern := range files {
+		t.Run("end-to-end_"+file, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				body, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					t.Error(err)
+				}
+				var data []map[string]string
 
-		if err != nil {
-			t.Errorf("Error during format discovery with file: %s", logfile)
-		}
+				if err := json.Unmarshal(body, &data); err != nil {
+					t.Error(err)
+				}
+				if len(data) != 1000 {
+					t.Error(err)
+				}
+				for _, b := range Map(data, assertReqFields) {
+					if !b {
+						t.Error()
+					}
+				}
+			}))
 
-		if expFmt != recFmt {
-			t.Errorf("Wrong format discovered for file: %s, expected: %s but got: %s",
-				logfile, expFmt, recFmt)
-		}
+			tf, err := tail.TailFile(file, tail.Config{Follow: true})
+			if err != nil {
+				t.Error(err)
+			}
+			g, err := grok.New(grok.Config{})
+			if err != nil {
+				t.Error(err)
+			}
+			cg, err := g.Compile(pattern)
+			if err != nil {
+				t.Error(err)
+			}
+			buffer := Buffer{data: make([]map[string]string, 0)}
+			go buffer.parseLines(ts.URL, tf, cg)
+			time.Sleep(10000 * time.Millisecond)
+			err = tf.Stop()
+			if err != nil {
+				t.Error(err)
+			}
+			defer ts.Close()
+		})
+	}
+}
+
+// TestPatternDiscovery makes sure the right pattern is
+// discovered for supported log patterns (at the moment:
+// apache common and combined)
+func TestPatternDiscovery(t *testing.T) {
+	files := map[string]string{
+		"tests/apache.log":          "%{COMMONAPACHELOG}",
+		"tests/apache_combined.log": "%{COMBINEDAPACHELOG}",
+		"tests/common_log.log":      "%{COMMONAPACHELOG}",
+		"tests/file.log":            "%{UNKNOWNPATTERN}",
+	}
+	for logfile, expPattern := range files {
+		t.Run("pattern-discovery_"+logfile, func(t *testing.T) {
+			recPattern, err := PatternDiscovery(logfile)
+
+			if err != nil {
+				t.Errorf("Error during format discovery with file: %s", logfile)
+			}
+
+			if expPattern != recPattern {
+				t.Errorf("Wrong format discovered for file: %s, expected: %s but got: %s",
+					logfile, expPattern, recPattern)
+			}
+		})
 	}
 }
